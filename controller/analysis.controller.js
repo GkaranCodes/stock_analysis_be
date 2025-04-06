8db444144c0998640be636e5c75285be8e9bf128
GkaranCodes <gkaran2112@gmail.com>
2025-04-06 15:47:08 +0530

    add edit and delete controller

--- controller/analysis.controller.js
+++ controller/analysis.controller.js
@@ -14,16 +14,54 @@ export const createAnalysis = async (req, res) => {
   }
 };
 
-export const getAnalysis = async (req, res)=>{
-    try {
-        const data = await StockAnalysis.find();
+export const getAnalysis = async (req, res) => {
+  try {
+    const data = await StockAnalysis.find();
 
-        if(!data){
-            return res.status(404).json({message:"Data not found", data:null});
-        }
-
-        return res.status(200).json({message:"Data fetched!", data: data});
-    } catch (error) {
-        return res.status(500).json({message:"Something went wrong", data:null});
+    if (!data) {
+      return res.status(404).json({ message: "Data not found", data: null });
     }
-}
+
+    return res.status(200).json({ message: "Data fetched!", data: data });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Something went wrong", data: null });
+  }
+};
+
+export const deleteAnalysis = async (req, res) => {
+  try {
+    const id = req.query.id;
+    await StockAnalysis.deleteOne({ _id: id });
+    return res.status(200).json({ message: "Entry Deleted Successfully." });
+  } catch (error) {
+    return res.status(500).json({ message: "Failed to delete record" });
+  }
+};
+
+export const editAnalysis = async (req, res) => {
+  try {
+    const id = req.query.id;
+    const body = req.body;
+
+    const updatedData = await StockAnalysis.findByIdAndUpdate(
+      id,
+      { $set: body },
+      { new: true } 
+    );
+
+    if (!updatedData) {
+      return res.status(404).json({ message: "Entry not found" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Entry updated successfully", data: updatedData });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Failed to update record", error: error.message });
+  }
+};
+
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import "dotenv/config.js"
 import dbConnect from "./db/dbConnect.js";
-import { createAnalysis, getAnalysis } from "./controller/analysis.controller.js";
+import { createAnalysis, deleteAnalysis, editAnalysis, getAnalysis } from "./controller/analysis.controller.js";
 import cors from "cors";
 
 dbConnect();
@@ -19,6 +19,7 @@ app.get("/testing", (req, res)=>{
 
 app.post("/create-analysis", createAnalysis);
 app.get("/get-analysis", getAnalysis);
- 
+app.delete("/delete-analysis", deleteAnalysis);
+app.put("/edit-analysis", editAnalysis);
 
 app.listen(process.env.PORT, ()=> console.log("Server running at 8080"));
